afc842cd10d3e50c4f5c05dcfe42f313a993085a
ivanbeltram <111466743+ivanbeltram@users.noreply.github.com>
2023-05-10 23:27:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+class Aplicacion{
+    constructor(){
+        this.canchasRegistradas = []
+    }
+    disponibilidadDiaHorario(hora, dia, mes){
+
+    }
+
+}
+
 class Cancha {
-    constructor(nombre, direccion, tamanio){
+    constructor(nombre, direccion, tamanio, precio, horariosAtencion){
         this.nombre = nombre,
         this.direccion = direccion,
         this.tamanio = tamanio,
-        this.calendario2023 = this.setCalendario2023()
+//      Si el horario de atención es de 14 a 24, se computa como 1400 a 2300, ya que cuenta desde el inicio de la última hora
+        this.horariosAtencion = horariosAtencion,
+        this.calendario2023 = this.setCalendario2023(horariosAtencion),
+        this.precio = precio
     }
 
-    setCalendario2023(){
+    setCalendario2023(horariosAtencion){
         let cantMeses = 12;
         let meses = [];
         for (let i = 0; i < cantMeses; i++){
@@ -65,49 +78,101 @@ class Cancha {
 
         for (let i = 0; i < meses.length; i++){
             for (let j = 0; j < meses[i].length; j++){
-                this.setDia(meses[i][j]);
+                this.setDia(meses[i][j],horariosAtencion);
             }
         }
 
         return meses;
     }
 
-    setDia(dia){
-        for (let i = 0; i < 10; i++){
-            dia.push(new Hora((1400 + (i*100)),this.tamanio*2));
+    setDia(dia,horariosAtencion){
+        for (let i = 0; i < horariosAtencion[1]; i++){
+            dia.push(new Hora((horariosAtencion[0] + (i*100)),this.tamanio*2));
         }
     }
 
     numHoraToPosision(numHora){
-        return (numHora - 1400) / 100;
+        return (numHora - this.horariosAtencion[0]) / 100;
     }
 
-    otorgarReserva(mes, dia, numHora, cantJugadores){
-        if(this.calendario2023[mes][dia][this.numHoraToPosision(numHora)].reservar(cantJugadores)){
-            console.log("Se ha registrado su reserva.");
+    otorgarReserva(mes, dia, numHora, titular){
+        if(numHora >= this.horariosAtencion[0] && numHora <= this.horariosAtencion[1]){
+            let nuevaReserva = this.calendario2023[mes][dia][this.numHoraToPosision(numHora)].reservar(titular, numHora);
+            if(nuevaReserva !== null){
+                titular.reservas.push[nuevaReserva];
+                console.log("Se ha registrado su reserva.");
+            } else {
+                console.log("No se ha podido efectuar su reserva porque la cancha ya está reservada en ese horario.");
+            }
+        } else{
+            console.log("Esta cancha no abre en la hora solicitada.");
+        }
+    }
+
+    getDisponibilidad(mes, dia, horaInicio, horaFin){
+        let horasDisponibles = [];
+        // Hay que chequear que exista al menos una hora de disponibilidad, por lo que evaluamos si el fin del horario
+        // de interés es más tarde que mi hora de apertura, o que el comienzo del horario de interés es más temprano que
+        // la hora de cierre.
+        if(horaFin >= this.horariosAtencion[0] || horaInicio <= this.horariosAtencion[1]){
+            let horaComunInicio;
+            let horaComunFin;
+            if(horaInicio >= this.horariosAtencion[0]){
+                horaComunInicio = horaInicio;
+            } else {
+                horaComunInicio = this.horariosAtencion[0];
+            }
+            if(horaFin <= this.horariosAtencion[1]){
+                horaComunFin = horaFin;
+            } else {
+                horaComunInicio = this.horariosAtencion[1];
+            }
+            for (let i = this.numHoraToPosision(horaComunInicio); i <= this.numHoraToPosision(horaComunFin); i++) {
+                if(this.calendario2023[mes][dia][i].estaDisponible()){
+                    console.log(this.calendario2023[mes][dia][i].numHora);
+                    horasDisponibles.push[this.calendario2023[mes][dia][i]];
+//                    console.log(horasDisponibles[horasDisponibles.length-1].numHora);
+                }
+            }
         } else {
-            console.log("No hay suficiente disponibilidad para efectuar su reserva.")
+            console.log("La cancha no está abierta en ese rango horario.");
+        }
+        return horasDisponibles;
+    }
+
+/*     getDisponibilidad(mes, dia){
+        this.getDisponibilidad(mes, dia, 0, 2300);
+    }
+ */
+    listarDisponibilidad(mes, dia, horaInicio, horaFin){
+        const disponibilidad = this.getDisponibilidad(mes, dia, horaInicio, horaFin);
+        console.log("hola2");
+        if(disponibilidad.length != 0){
+            console.log("hola3");
+            for(let i = 0; i < disponibilidad.length; i++){
+                console.log(disponibilidad[i].numHora);
+            }
         }
     }
 }
 
 class Hora {
-    constructor(numHora, disponibilidad){
+    constructor(numHora){
         this.numHora = numHora,
-        this.disponibilidad = disponibilidad,
-        this.reservas = []
+        this.reserva = null
     }
 
-    hayDisponibilidad(cantJugadores){
-        return this.disponibilidad >= cantJugadores;
+    estaDisponible(){
+        return this.reserva === null;
     }
 
-    reservar(cantJugadores){
-        let hayDisponibilidad = this.hayDisponibilidad(cantJugadores);
-        if (hayDisponibilidad){
-            this.disponibilidad -= cantJugadores;
+    reservar(titular, horaInicio){
+        if (this.estaDisponible()){
+            this.reserva = new Reserva(titular, horaInicio);
+            return this.reserva;
+        }else{
+            return null;
         }
-        return hayDisponibilidad;
     }
 }
 
@@ -116,20 +181,41 @@ class Usuario {
         this.nombre = nombre,
         this.reservas = []
     }
+}
 
-    reservar(reserva){
-        this.reservas.push(reserva);
+class UsuarioDueño extends Usuario {
+    constructor(nombre, canchas){
+        super(nombre),
+        this.canchas = canchas
     }
 }
 
+class UsuarioJugador extends Usuario {
+    constructor(nombre){
+        super(nombre),
+        this.reservas = []
+    }
+
+    reservar(cancha, mes, dia, numHora){
+        cancha.otorgarReserva(mes, dia, numHora, this);
+    }
+
+}
+
 class Reserva {
-    constructor(titular, cantidadJugadores, horaInicio){
+    constructor(titular, horaInicio){
         this.titular = titular,
-        this.cantidadJugadores = cantidadJugadores,
         this.horaInicio = horaInicio
     }
 }
 
-let cancha = new Cancha("El último 10", "Av. Mitre 1234, Caseros", 5);
-cancha.otorgarReserva(0,0,1400,11);
-cancha.otorgarReserva(0,0,1400,1);
+let cancha = new Cancha("El último 10", "Av. Mitre 1234, Caseros", 5, 1500, [1400,2300]);
+let jugador1 = new UsuarioJugador("Jaime")
+jugador1.reservar(cancha,0,0,1400);
+jugador1.reservar(cancha,0,0,1400);
+jugador1.reservar(cancha,0,0,2000);
+//cancha.otorgarReserva(0,0,1000);
+//cancha.otorgarReserva(0,0,1400);
+//cancha.otorgarReserva(0,0,1400);
+
+cancha.listarDisponibilidad(0,0,0,2300);

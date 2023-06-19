522ecf42611577dc667aa537d503acdabbb3154e
ivanbeltram <ivan97beltram@gmail.com>
2023-06-19 04:37:32 -0300

    Se agrega configuración de módulo cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const express = require("express");
+const cors = require("cors");
 const { conectar } = require("./database");
 const bodyParser = require("body-parser");
 const app = express();
@@ -13,6 +14,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
